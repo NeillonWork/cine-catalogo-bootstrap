f6d2e37e276720e64eadef485e831505a5c5cbcf
Admin <neillonwork@gmail.com>
2024-04-17 14:44:52 -0300

    criado Home page, Header, pagina de Erro

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
-    <div className="container">
-        <h1>React Bootstrap</h1>
-
-        <button className="btn btn-primary mb-5">Clique em Mim</button>
-        <table class="table table-dark table-hover">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+<div>OLA</div>
   );
 }
 
