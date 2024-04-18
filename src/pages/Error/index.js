a848b6d5152b91e1ab7dad409667645c45f185e5
Admin <neillonwork@gmail.com>
2024-04-18 19:08:05 -0300

    melhorado visual da pagina de erro

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+
 function Error() {
   return (
-    <div className="card">
-     <h1>ERRO</h1>
+    <div className="container d-flex justify-content-center">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">404 - Essa pagina nao existe</h1>
+          <p class="lead">Links disponiveis:</p>
+          <ul className="pb-5">
+            <li>
+              <Link className="text-decoration-none" to="/">
+                Catalogo de FIlmes
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none" to="/favoritos">
+                Favoritos
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
